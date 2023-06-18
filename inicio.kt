151b27616b2e6b266753ffb94e9663961ddf073e
shencanet <santidionis@Mac-mini-de-Santi.local>
2023-06-18 17:30:15 +0200

    first commit

---
+++ inicio.kt
@@ -0,0 +1,9 @@
+var matriz = arrayof(
+    intArrayOf(1,2.3),
+    intArrayOf(4,5,6,7,8,9,10),
+    intArrayOf(11,12,13)
+)
+for  (i in (0 until matriz.size)){
+    println()
+    for (j in (0 until ))
+}
