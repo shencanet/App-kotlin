7677eea9b96daa3148599190f954372fb66cb283
shencanet <santi.dionis@gmail.com>
2023-06-18 19:31:10 +0200

    commit message

--- inicio.kt
+++ inicio.kt
@@ -11,17 +11,21 @@ for  (i in (0 until matriz.size)){
 }
 
 //colecciones set y colecciones lista
-//mutables y inmutables
-
+//mutables 
 var clientesVIP : Set<Int> = setOf (1234, 5678, 4040) //datos ser de datos enteros
 val setMezclado : setOf(2, 4.458, "pedro", 'c')
 println("clientes VIP: \n") 
 println(clientesVIP)
+
 println("Numero de Clientes VIP${clientesVIP.size} ")
 
 if(clientesVIP.contains(1234))println("cliente Vip encontrado 1")
 
 if(clientesVIP.contains(1111))println("cliente Vip encontrado 2")
+
+
+//version mutable
+
 
 
 }
