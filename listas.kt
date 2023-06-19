563896543dbe3b1e3ab8620fc8c75de0e554822c
shen <santi.dionis@gmail.com>
2023-06-19 20:45:24 +0200

    ADD: curso kotlin

--- listas.kt
+++ listas.kt
@@ -13,13 +13,19 @@ println(mapa)
 
 //listas mutables
 
-var mapa2: MutableMap<Int, String> = mutableMapOf(
-    1 to "Lunes",
-    2 to "Martes",
-    3 to "Miercoles",
-    4 to "Jueves",
-    5 to "Viernes",
-    6 to "Sabado",
-    7 to "Domingo"
-)
+var inversiones = mutableMapOf<String, Float>()
+    inversiones.put("Google", 12.5f)
+    inversiones.put("Apple", 15.5f)
+    inversiones.put("Amazon", 17.5f)
+    inversiones.put("Tesla", 19.5f)
+    inversiones.put("Microsoft", 21.5f)
+    inversiones.put("Facebook", 23.5f)
+    inversiones.remove("Tesla")
+
+println(inversiones)
+
+
+
+
+
 
