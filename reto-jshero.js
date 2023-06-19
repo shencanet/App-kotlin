51d2534018d1fd675032534ed0f2db31ac9059bc
shen <santi.dionis@gmail.com>
2023-06-19 20:31:48 +0200

    ADD: reto JS Hero

--- reto-jshero.js
+++ reto-jshero.js
@@ -6,3 +6,9 @@ let Alimento = function (comida) {
   const alimento = new Alimento('ensalada');
   
   alimento.comerHoy(); //❓
+/*Respuesta Correcta: D) Hoy toca comer: undefined
+Una de las limitaciones de las funciones flecha (arrow functions), es que no tiene sus propias referencias a this o super y no se debe usar como métodos.
+
+this siempre apuntará a this desde el contexto en el que definimos la función (ejemplo window).
+
+Por tal razón, la función flecha () => { ... } no sabe de this.comida, por lo que this.comida es undefined.*/
