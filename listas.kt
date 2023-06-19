232ba4c56b03300450ed5f16ec65f07743128146
shen <santi.dionis@gmail.com>
2023-06-19 20:37:30 +0200

    ADD: curso kotlin

---
+++ listas.kt
@@ -0,0 +1,10 @@
+var mapa: Map<Int, String> = mapOf(
+    1 to "Lunes",
+    2 to "Martes",
+    3 to "Miercoles",
+    4 to "Jueves",
+    5 to "Viernes",
+    6 to "Sabado",
+    7 to "Domingo"
+)
+println(mapa)
