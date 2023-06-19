80d5df69d47fdd2737d82fdec75233db112b9670
shen <santi.dionis@gmail.com>
2023-06-19 20:40:19 +0200

    ADD: curso kotlin

--- listas.kt
+++ listas.kt
@@ -1,3 +1,5 @@
+
+//listas no mutables
 var mapa: Map<Int, String> = mapOf(
     1 to "Lunes",
     2 to "Martes",
@@ -8,3 +10,16 @@ var mapa: Map<Int, String> = mapOf(
     7 to "Domingo"
 )
 println(mapa)
+
+//listas mutables
+
+var mapa2: MutableMap<Int, String> = mutableMapOf(
+    1 to "Lunes",
+    2 to "Martes",
+    3 to "Miercoles",
+    4 to "Jueves",
+    5 to "Viernes",
+    6 to "Sabado",
+    7 to "Domingo"
+)
+
