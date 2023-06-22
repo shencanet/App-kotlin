470e217eaa685dbd426c4e97fb9fa66110c0b942
shen <santi.dionis@gmail.com>
2023-06-22 20:38:45 +0200

    ADD: curso kotlin

--- reto-jshero.js
+++ reto-jshero.js
@@ -52,3 +52,11 @@ sayHi();
 La función greeting con la palabra reservada throw genera una excepción de tipo string en el código.
 
 La función sayHi consta de una sentencia try...catch, recordemos que si no hay ningún tipo de excepción el código ejecuta el bloque try pero como si generamos una excepción entonces entramos al bloque catch donde el parámetro e adopta el valor de la excepción, osea, Hello world!. Por eso el resultado es "Oh no an error! Hello world!"/
+
+*/
+const arrl = [2, 4, 6];
+
+const arr2 =[3, 5, 7];
+
+
+console.log([...arr1, ...arr2]);
