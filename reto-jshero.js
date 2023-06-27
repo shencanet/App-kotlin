be35043dcfd81b526a31ca6a7da0fd827869a053
shen <santi.dionis@gmail.com>
2023-06-27 18:56:22 +0200

    ADD: reto JS Hero

--- reto-jshero.js
+++ reto-jshero.js
@@ -99,3 +99,5 @@ console.log(checkAge(21)); //🤔?
 
 /*Respuesta Correcta: C) ReferenceError
 const tiene scope de bloque para las variables, cuando intentamos hacer return message la variable message no puede ser accedida. Tanto message en el bloque if como en el else son variables diferentes por que estan en bloques diferentes pese a que se llaman igual. Como no es posible acceder a la variable la respuesta es ReferenceError.*/
+
+console.log('I want pizza'[0]); //🤔?
