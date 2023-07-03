93d5e32203c2bc4044e6e49802031d331e978ed7
shen <santi.dionis@gmail.com>
2023-07-03 21:05:05 +0200

    ADD: curso kotlin accesos directos

--- atajos teclado studio.kt
+++ atajos teclado studio.kt
@@ -5,5 +5,10 @@ control + shift + F = buscar en todos los archivos
 control + shift + R = reemplazar en todos los archivos
 alt + f7 = buscar usos de la variable
 control + shift + F7 = resaltar usos de la variable
- 
+control + click = ir a la definicion de la variable
+control + shift + click = ir a la definicion de la variable en una nueva pestaña
+control + alt + click = ir a la definicion de la variable en una nueva pestaña
+control + alt + b = ir a la implementacion de la funcion
+control + alt + click = ir a la implementacion de la funcion en una nueva pestaña
+
 
