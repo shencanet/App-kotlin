c2af0e016e7b386ad48fde692909ec858d4f4f55
shen <santi.dionis@gmail.com>
2023-07-03 20:59:35 +0200

    ADD: curso kotlin Lambdas

---
+++ atajos teclado studio.kt
@@ -0,0 +1,9 @@
+/*atajos busqueda android studio
+control + F = buscar
+control + R = reemplazar
+control + shift + F = buscar en todos los archivos
+control + shift + R = reemplazar en todos los archivos
+alt + f7 = buscar usos de la variable
+control + shift + F7 = resaltar usos de la variable
+ 
+
