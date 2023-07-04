07ed6e35e2e80f1b2d1f5d9941c0ce929a948c35
shen <santi.dionis@gmail.com>
2023-07-04 21:59:14 +0200

    ADD: reto JS Hero

--- reto-jshero.js
+++ reto-jshero.js
@@ -434,3 +434,16 @@ const person = {
 for (const [x, y] of Object.entries(person)) {
   console.log(x, y); //🤔?
 }
+
+/*Respuesta Correcta: A) name Lydia y age 21
+El método entries del constructor Object regresa un arreglo anidado donde cada sub arreglo corresponde a la llave y valor del objeto:
+
+  [ [ "name", "Lydia" ], [ "age", 21 ] ]  
+
+
+
+Con el bucle for...of iteramos sobre el objeto desestructurando los valores con la sintaxis [x, y].
+
+El primer sub arreglo es [ "name", "Lydia" ] donde x toma el valor name y y toma el valor Lydia.
+
+El segundo arreglo es [ "age", 21 ] donde x toma el valor age y y toma el valor 21.*/
