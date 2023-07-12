c162b76448c1e8d479164fa687963e0d9ccec227
shen <santi.dionis@gmail.com>
2023-07-12 20:25:10 +0200

    ADD: Rero JShero Platzi

--- reto-jshero.js
+++ reto-jshero.js
@@ -695,3 +695,7 @@ function compareMembers(person1, person2 = person) {
 
 const person = { name: 'Lydia' };
 compareMembers(person);
+/*Respuesta Correcta: B) They are the same!
+Tanto el parámetro person1 como person2 adoptará el valor de person, osea el objeto { name: "Lydia" }.
+
+Los objetos se pasan por referencia. En el ejemplo, person1 y person2 apuntan a la misma dirección de memoria entonces la condición del if no se cumple y pasamos a imprimir They are the same!./
