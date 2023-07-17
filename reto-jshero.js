4cdb198ae8cbd9fcda96679a564d418e051602b6
shen <santi.dionis@gmail.com>
2023-07-17 20:42:08 +0200

    ADD: Rero JShero Platzi

--- reto-jshero.js
+++ reto-jshero.js
@@ -770,3 +770,19 @@ En ningún momento modificamos de ninguna manera el array food, por ende sigue s
 console.log(`${((x) => x)('I love')} to program`);
 
 
+/Respuesta Correcta: A) I love to program
+Al usar los backtiks de ES6, las expresiones se evaluan primero.
+
+En este caso la expresión completa es:
+
+  ${(x => x)("I love")}  
+
+
+/*
+Donde:
+
+(x => x) es una función anónima de tipo flecha, que recibe un parámetro x y con un return implícito lo devuelve.
+
+("I love") es la llamada a la función anónima, acá pasamos como argumento a la función la cadena I love.
+
+Entonces, la función es llamada y regresa únicamente el parámetro que se le pasa. Por ello la respuesta es I love to program.*/
