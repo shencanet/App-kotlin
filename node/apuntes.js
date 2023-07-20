fade3be96d4618f3e476129d3122abc9fe1ade5b
shen <santi.dionis@gmail.com>
2023-07-20 21:17:17 +0200

    ADD: curso midudev node

---
+++ node/apuntes.js
@@ -0,0 +1,11 @@
+/*node es un entorno de de ejecucion de Javascript
+construido con el motor de Javascript V8 de Chrome
+podemos ejecutar codigo de Javascript fuera del navegador Vamos en el backend
+
+npm es el gestor de paquetes de Node.js
+codigo abierto
+miltiplataforma
+orientado a eventos
+asincrono
+event loop
+
