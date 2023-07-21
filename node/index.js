06460daea2c570b3abd791255ce5b6300f74730f
shen <santi.dionis@gmail.com>
2023-07-21 21:19:24 +0200

    ADD: curso midudev node

--- node/index.js
+++ node/index.js
@@ -3,3 +3,11 @@ console.info("Hola Mundo")
 console.error("Hola Mundo")
 console.warn("Hola Mundo")
 console.table("Hola Mundo")
+//console.log(window)//da error porque no existe window en node
+console.log(global)//global this es una variable global en toda nuestra aplicacion--> apunta a window en el navegador
+//GLOBAL <----NODE  -----< GLOBALTHIS >----->NAVEGADOR---->WINDOW NAVEGADOR APUNTA A WINDOW PERO EN NODE APUNTA A GLOBAL
+globalThis.console.log("Hola Mundo")
+//TODO LO QUE ESTA EN GLOBALTHIS ESTA EN GLOBAL Y VICEVERSA
+console.log(globalThis.XXXXXXXXX)//globalThis es una variable global en toda nuestra aplicacion--> apunta a window en el navegador
+//PATRONES DISEÑO MODULO
+//DIFERENTES MODULOS Y REUTILIZARLO
