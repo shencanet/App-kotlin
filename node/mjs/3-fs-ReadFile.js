81456726827a8d6f54c90739192334f423c11ae7
shen <santi.dionis@gmail.com>
2023-07-22 20:06:16 +0200

    ADD: curso midudev node

--- node/mjs/3-fs-ReadFile.js
+++ node/mjs/3-fs-ReadFile.js
@@ -1,11 +1,28 @@
-const fs = require('node:fs')
-const { test } = require('node:test')
+const fs = require('node:fs/promises')
+//const { promises: fs } = require('node:util')//igual que la anterior para compaginar ambas versiones promesas callback
+//const { test } = require('node:test')
 
 console.log('leyendo el primer archivo......')
-const test = fs.readFileSync('./archivo.txt', 'utf-8' (err, test))//sincrona callback asincrona
-console.log(test)
+
+fs.readFile('./archivo.txt', 'utf-8')
+.then(text => {
+    console.log('Text Promesas', text)
+
+})
+
+
+
+
+
+
+console.log("Moviendo las manitas")
 
 console.log('leyendo el segundo archivo......')
-const test2 = fs.readFileSync('./archivo2.txt', 'utf-8')
+fs.readFile('./archivo2.txt', 'utf-8',(err, text) => {
+    console.log(text)
+})
 
-console.log(test2)
+/*
+console.log('leyendo el tercer archivo......')
+const text = fs.readFileSync('./archivo3.txt', 'utf-8')//sincrona
+console.log(text)*/
