8d4574309bc7d1c7fb8c4d50de0d3fe26011fd22
shen <santi.dionis@gmail.com>
2023-07-23 20:09:43 +0200

    ADD: curso midudev node

--- JavaScript/reto-jshero2.js
+++ JavaScript/reto-jshero2.js
@@ -187,3 +187,14 @@ Hoisting es una técnica de desplazamiento de declaraciones de variables y funci
 El concepto de Hoisting fue pensado como una manera general de referirse a cómo funcionan los contextos de ejecución en JavaScript
 
 Conceptualmente, por ejemplo, una estricta definición de hoisting sugiere que las declaraciones de variables y funciones son físicamente movidas al comienzo del código, pero esto no es lo que ocurre en realidad. Lo que sucede es que las declaraciones de variables y funciones son asignadas en memoria durante la fase de compilación, pero quedan exactamente en dónde las has escrito en el código. (mdn)*/
+
+const banda = {
+  id: 1,
+  nombre: 'Radiohead',
+  'tipo de musica': 'Rock',
+  albunes: ['Pablo Honey', 'Ok Computer', 'In Rainbows'],
+};
+banda.vocalista = undefined;
+console.log('vocalista' in banda);
+delete banda['tipo de musica'];
+console.log('tipo de musica' in banda);
