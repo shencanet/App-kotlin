4df99977a3467ca34c0962af16ecd3e4db12b609
shen <santi.dionis@gmail.com>
2023-07-24 21:00:28 +0200

    ADD: Rero JShero Platzi

--- node/mjs/3-fs-ReadFile.js
+++ node/mjs/3-fs-ReadFile.js
@@ -35,3 +35,11 @@ const banda = {
     albunes: ['Pablo Honey', 'Ok Computer', 'In Rainbows'],
   };
   console.log(banda['nom' + 'bre']);
+  /*Respuesta Correcta: A) Radiohead
+En JavaScript hay dos maneras de acceder a las propiedades de un objeto, con la notación de punto y con la notación de corchetes.
+
+Usamos la notación de punto cuando conocemos el nombre literal de la propiedad a la que queremos acceder. La key a la que accedemos con esta notación debe ser un nombre de variable válido.
+
+La notación de corchetes se diferencia en que todo lo que este dentro de los corchetes debe ser un string y es evaluado por JavaScript como una expresión.
+
+Por este motivo, cuando hacemos console.log(banda["nom"+"bre"]) el lenguaje evalua los corchetes concatenando las cadenas de texto y mostramos por consola Radiohead.*/
