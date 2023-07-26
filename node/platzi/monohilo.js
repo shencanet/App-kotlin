5978ab077f2647bb73ff60a30bcb723488ae9a10
shen <santi.dionis@gmail.com>
2023-07-26 20:40:57 +0200

    ADD: curso midudev node

---
+++ node/platzi/monohilo.js
@@ -0,0 +1,16 @@
+console.log('Hola mundo')
+
+setInterval(function(){
+    console.log('Sigo activo')
+},1000)
+
+let i = 0;
+
+setInterval(function(){
+console.log(i);
+i++;
+
+if(i === 5){
+    var a = 3 + z;
+}
+}, 100);
