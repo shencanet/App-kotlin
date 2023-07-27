f4154a7a9046aff092c2f948d986e48be25dda12
shen <santi.dionis@gmail.com>
2023-07-27 19:16:20 +0200

    ADD: curso midudev node

--- node/Ficheros midu clase 1/curso-node-js-main/clase-2/1https.js
+++ node/Ficheros midu clase 1/curso-node-js-main/clase-2/1https.js
@@ -1,18 +1,28 @@
 const http = require('node:http') // protocolo HTTP
 const { findAvailablePort } = require('./10.free-port.js')
-const { lstat } = require('node:fs')
+const fs = require('node:fs') // file system
+
 
 console.log(process.env)//variable entorno
 
-const desiredPort = process.env.PORT ?? 3000
+const desiredPort = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {//creamnos el servidos recibe un callback
-  console.log('request received', req.url)//url de la peticion
-  res.end('Hola mundo')
-})
+const processRequest = (request, response) => {
+  if(request.url === '/') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html', charset='utf-8')
+    response.end('Hello World')
+    response.end()
+  }else if(request.url === '/contacto') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html', charset='utf-8')
+    response.end('<h1>About</h1>')
+    response.end()
+}else {
+  request.statusCode = 404
+  response.setHeader('Content-Type', 'text/html', charset='utf-8')
+  response.end('Not Found')
+  response.end()
 
-findAvailablePort(desiredPort).then(port => {
-  server.listen(port, () => {
-    console.log(`server listening on port http://localhost:${port}`)
-  })
-})
+}
+}
