63d784704064ccda581e91b658b6e0d1710583c6
shen <santi.dionis@gmail.com>
2023-07-28 21:23:58 +0200

    ADD: curso node platzi

---
+++ node/platzi/modulos/fs.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises
+
+async function escribir(path, contenido, codificacion){
+    try {
+        await fs.writeFile(path, contenido, codificacion)
+        console.log('Archivo escrito con exito')
+    } catch (error) {
+        console.error('No se pudo escribir el archivo', error)
+    }
+}
+
+async function leer(path, codificacion){
+
+    try{
+        const data = await fs.readFile(path, codificacion);
+        returndata.toString();+ç
+
+    }catch(error){
+        console.error('No se pudo leer el archivo', error)
+    }
+}
