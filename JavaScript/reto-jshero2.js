3a5840faa6863a45d6899291221e51ab92b57b41
shen <santi.dionis@gmail.com>
2023-07-29 21:22:42 +0200

    ADD: curso node platzi

--- JavaScript/reto-jshero2.js
+++ JavaScript/reto-jshero2.js
@@ -307,4 +307,10 @@ console.log(canciones['len' + 'gth']);
 /*Respuesta Correcta: C) 8, 3
 Tanto cadenas como arreglos son iterables, entonces podemos usar la nomenclatura de corchetes para acceder a sus valores.
 
-Todo lo que este dentro de los corchetes será evaluado como expresión, entonces ambos casos se ejecutaran correctamente, el primero solo ejecuta el método length y el segundo concatena ambas cadenas para finalmente ejecutar length para el arreglo./
+Todo lo que este dentro de los corchetes será evaluado como expresión, entonces ambos casos se ejecutaran correctamente, el primero solo ejecuta el método length y el segundo concatena ambas cadenas para finalmente ejecutar length para el arreglo./*/
+
+//-----------------------------------------------------------------------------------------------------------
+
+console.log(undefined || '0' || null || (undefined ?? 0));
+
+
