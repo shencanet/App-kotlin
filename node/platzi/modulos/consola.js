0fb03b93d1b7e4131752b6f067068aafd9e15400
shen <santi.dionis@gmail.com>
2023-07-29 21:45:01 +0200

    ADD: curso node platzi

---
+++ node/platzi/modulos/consola.js
@@ -0,0 +1,20 @@
+console.log('--------------------------------------------------');
+console.info('Clase 10: Modulos');
+console.error('error de prueba');
+console.warn('warning de prueba');
+console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }]);
+console.group('grupo 1');
+
+
+console.group("conversacion 1");
+console.log('Hola');
+console.log('Blablabla');
+console.log('Adios');
+console.groupEnd("conversacion 1");
+console.log('otra cosa de otra funcion');
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
