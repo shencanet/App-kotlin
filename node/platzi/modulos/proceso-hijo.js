311a3d5f2737837226c7fd50187225624c74aad3
shen <santi.dionis@gmail.com>
2023-07-30 15:43:01 +0200

    ADD: curso node platzi

--- node/platzi/modulos/proceso-hijo.js
+++ node/platzi/modulos/proceso-hijo.js
@@ -1,7 +1,9 @@
 const { exec } = require('child_process');
 //const exec = require('child_process').exec// ñas dos lineas hacen lo mismo
+
 //exec('ls -ls', (err, stdout, sterr) linux
-exec('dir', (err, stdout, sterr)=> {//windows
+//exec('dir', (err, stdout, sterr)=> {//windows
+exec('node consola.js', (err, stdout, sterr) => { //llamamos a un porceso hijp de otro modulo
     if (err){
         console.log(err);
         return false;
