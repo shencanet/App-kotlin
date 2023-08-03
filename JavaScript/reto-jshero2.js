4dc3d1a88c7fb8ce102b5f2dc9d1f794c72fed55
shen <santi.dionis@gmail.com>
2023-08-03 21:08:42 +0200

    ADD: Rero JShero Platzi

--- JavaScript/reto-jshero2.js
+++ JavaScript/reto-jshero2.js
@@ -404,3 +404,11 @@ En este caso usar length y "setterlo" a 0 es una buena manera de borrar todos lo
 const arr = [];
 console.log(Array.isArray(arr)); // ??
 
+/*Respuesta Correcta: A) true
+Una manera adecuada de comprobar que un arreglo es efectivamente un arreglo es usar el constructor Array con su método isArray.
+
+Como arr es un arreglo (vacío pero arreglo al fin), entonces regresamos true.
+
+Como los arreglos no son un tipo de dato per se en JavaScript, la mejor manera de comprobar si un arreglo es un arreglo es de esta manera.
+
+¿Te cuento un secreto? Esta pregunta es bastante frecuente en entrevistas laborales, pero shhh, no se lo digas a nadie 👀*/
