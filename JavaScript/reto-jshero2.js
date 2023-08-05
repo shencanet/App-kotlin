0d1197adcb07acda11377927a3dd8ef0bdfa99fd
shen <santi.dionis@gmail.com>
2023-08-05 21:02:24 +0200

    ADD: Rero JShero Platzi

--- JavaScript/reto-jshero2.js
+++ JavaScript/reto-jshero2.js
@@ -421,3 +421,9 @@ Pese a que null es un primitivo, debido a un bug del lenguaje su tipo de dato es
 Este bug es muy antiguo y se determino que no vale la pena arreglarlo al día de hoy ya que se pueden romper muchos programas que pueden depender de esta error.
 
 Este bug es bastante conocido en programadores experimentados y usado en entrevistas laborales para estimar tu conocimiento del lenguaje.*/
+
+//-----------------------------------------------------------------------------------------------------------
+const nombre = 'Alejandro';
+console.log(nombre.split(''));
+console.log([...nombre]);
+console.log(Array.from(nombre));
