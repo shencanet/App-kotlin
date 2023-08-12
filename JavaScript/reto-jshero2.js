3961d49233d53482ea681a392feb45080f1dc756
shen <santi.dionis@gmail.com>
2023-08-12 20:58:49 +0200

    ADD: Rero JShero Platzi

--- JavaScript/reto-jshero2.js
+++ JavaScript/reto-jshero2.js
@@ -540,3 +540,8 @@ Dentro de un objeto literal es posible usar nombres de palabras reservadas del l
 let nombre = 'Cris';
 let edad = 25;
 console.log('Mi nombre es %s y tengo %d', nombre, edad);
+
+Respuesta Correcta: C) Mi nombre es Cris y tengo 25
+En javascript como en java es posible usar sustituciones de variables con el operador % seguido de un caracter que especifica el tipo de dato que se pretende imprimir. En este caso, $s reemplaza un string ("Cris") y %d reemplaza un valor decimal o dígito numérico (25).
+
+Este método de imprimir por consola no es muy usado, ni siquiera es conocido, pero esta bueno saber que existe.
